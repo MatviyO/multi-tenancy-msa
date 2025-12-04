033b5f7949af0b789342d8616d86d56ff2917f79
Matvii Oleh <oleh.m97@gmail.com>
2025-12-04 22:18:04 +0200

    setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,59 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
+    es2022: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'import', 'sonarjs'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:sonarjs/recommended',
+    'prettier', // ВАЖЛИВО: останнім, щоб вимкнути конфлікти з Prettier
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  },
   rules: {
+    // TypeScript
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn', // warn замість off для кращої практики
+    '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    // Imports
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'import/no-default-export': 'off', // NestJS використовує default export
+    'import/no-unresolved': 'error',
+
+    // SonarJS
+    'sonarjs/no-duplicate-string': 'off', // може бути занадто шумним
+    'sonarjs/cognitive-complexity': ['warn', 15],
   },
+  ignorePatterns: ['.eslintrc.js', 'dist', 'coverage', 'node_modules'],
 };
---
+++ knip.ts
@@ -0,0 +1,6 @@
+const config = {
+  entry: ['src/main.ts'],
+  project: ['src/**/*.ts'],
+};
+
+export default config;
